32f384ac0b64cbb9825f0c9b312c3abaebd5c908
2100018090 <muhammad2100018090@webmail.uad.ac.id>
2024-09-23 07:38:07 +0700

    masuk adata

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+export default {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        custom: '#64C0B7',
+      },
+    },
+  },
+  plugins: [],
+}
